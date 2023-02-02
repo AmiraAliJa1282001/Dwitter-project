2aaa2bbddbbdba7b0f75c05d94e7b90d646027b3
AmiraAliJa1282001 <aa.mm.ii.rr.aa.1282001@gmail.com>
2023-02-02 19:09:46 +0200

    update

---
+++ social/dwitter/models.py
@@ -0,0 +1,24 @@
+
+# Create your models here.
+# dwitter/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+    "self", related_name="followed_by", symmetrical=False, blank=True
+    )
+    def __str__(self):
+        return self.user.username
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+# ...
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(instance.profile)
+        user_profile.save()
---
+++ social/dwitter/views.py
@@ -0,0 +1,30 @@
+
+
+# Create your views here.
+# dwitter/views.py
+from django.shortcuts import render
+from .models import Profile
+
+def dashboard(request):
+    return render(request, "base.html")
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "dwitter/profile_list.html", {"profiles": profiles})
+
+
+def profile(request, pk):
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
+    profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == "unfollow":
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()
+    return render(request, "dwitter/profile.html", {"profile": profile})
